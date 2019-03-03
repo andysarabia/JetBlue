ab0939a1fad88d516ff6dce7d019b02c460519aa
andysarabia <brandycsarabia@gmail.com>
2019-03-03 18:40:58 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("myForm").submit();
+  }
+
+function myFunction() { 
+	document.getElementById("mainFrameOne").style.display="none"; 
+	document.getElementById("mainFrameTwo").style.display="block"; 
+}
+
